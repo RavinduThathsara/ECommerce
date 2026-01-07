fb07c6bfc51266864e80287c630475e4b608cef9
RavinduThathsara <ravinduthathsara38@gmail.com>
2026-01-07 13:31:18 +0530

    user deleted reponse

--- controllers/userController.js
+++ controllers/userController.js
@@ -63,3 +63,13 @@ export function loginUser(req,res){
         }
     )
 }
+
+
+export function deleteUser(req,res){
+    User.deleteOne({email : req.body.email}).then(()=>{
+
+        res.json({
+            message : "User deleteed"
+        })
+    })
+}

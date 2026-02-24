1de76f23aa501b04839e80f5fbe9c0de406a6672
RavinduThathsara <ravinduthathsara38@gmail.com>
2026-02-24 17:07:46 +0530

    Restore backend structure and resolve configuration issues 🚨🚨

---
+++ controllers/orderController.js
@@ -0,0 +1,89 @@
+import Order from "../models/order.js";
+import { isCustomer } from "./userController.js";
+
+export async function createOrder(res, req) {
+    //cbcooo1
+    if (!isCustomer) {
+        res.json({
+            message: "please login as customer to crate orders"
+        })
+    }
+    //take the latest product id
+    try {
+        const latestOrder = await Order.find().sort({ date: -1 }).limit(1)
+
+        let orderId
+
+        if (latestOrder.length == 0) {
+            orderId = "CBC0001"
+        } else {
+            const currentOrderId = latestOrder[0].orderId
+
+            const numberString = currentOrderId.replace("CBC", "")
+            const number = parseInt(numberString)
+
+            const newNumber = (number + 1).toString().padStart(4, "0");
+
+            orderId = "CBC" + newNumber
+        }
+
+        const newOrderData = req.body
+
+        const newProductArray = []
+
+        for (let i = 0; newOrderData.orderedItems.length; i++) {
+
+            const product = await Product.findOne({
+                productId: newOrderData.orderedItem[i].productId
+            })
+
+            if (product == null) {
+                res.json({
+                    message: "Product not Found"
+                })
+
+                return
+            }
+
+            newProductArray[i] = {
+                name: product.productName,
+                price: product.price,
+                quentity: newOrderData.orderdItems[i].quantity,
+                image: product.images[0]
+            }
+
+
+        }
+
+        console.log(newProductArray)
+
+        newOrderData.orderedItem = newProductArray
+
+
+
+        newOrderData.orderId = orderId
+        newOrderData.email = req.user.email
+
+        const order = new Order(newOrderData)
+        await order.save()
+
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+
+export async function getOrders(req, res) {
+
+    try {
+        const orders = await Order.find({ email: req.user.email })
+        res.json(orders)
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
